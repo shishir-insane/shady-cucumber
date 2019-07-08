1628c15b6b886470ef97a9919bea2d5083a7c5a9
shishir-insane <shishir.insane@gmail.com>
2019-07-08 16:54:36 +0200

    First Light

---
+++ service/src/test/java/com/sk/spring/translator/ShadyTranslatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sk.spring.translator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShadyTranslatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
